30b2f4a4ec0866024379d21456070ca0253a6746
Lars Kellogg-Stedman <lars@oddbit.com>
2020-01-10 22:58:18 -0500

    Add additional tests
    We now test explicitly for most of the changes described in the doc.

--- ovn-and-dhcp/tests/test_common.py
+++ ovn-and-dhcp/tests/test_common.py
@@ -1,3 +1,5 @@
+import pytest
+
 testinfra_hosts = ['ansible://ovn']
 
 
@@ -14,3 +16,15 @@ def test_ovn_remote(host):
     res = host.run('ovs-vsctl get open_vswitch . external_ids:ovn-remote')
     assert res.exit_status == 0
     assert 'tcp:192.168.122.100:6642' in res.stdout
+
+
+def test_ovs_errors(host):
+    res = host.run('ovs-vsctl show')
+    assert 'error:' not in res.stdout
+
+
+@pytest.mark.parametrize('service',
+                         ['ovs-vswitchd', 'ovsdb-server', 'ovn-controller'])
+def test_services(host, service):
+    res = host.run(f'systemctl is-active {service}')
+    assert res.exit_status == 0
--- ovn-and-dhcp/tests/test_controller.py
+++ ovn-and-dhcp/tests/test_controller.py
@@ -10,6 +10,17 @@ ports = {
 }
 
 
+@pytest.mark.parametrize('service', ['ovn-northd'])
+def test_services(host, service):
+    res = host.run(f'systemctl is-active {service}')
+    assert res.exit_status == 0
+
+
+def test_ovnsb_connection(host):
+    res = host.run('ovn-sbctl get-connection')
+    assert 'ptcp:6642' in res.stdout
+
+
 def test_net0_exists(host):
     res = host.run('ovn-nbctl list logical_switch net0')
     assert res.exit_status == 0
@@ -35,3 +46,27 @@ def test_port_mac(host, port, mac):
 def test_port_bound(host, port):
     res = host.run(f'ovn-sbctl -t 5 list port_binding {port}')
     assert res.exit_status == 0
+
+
+def test_dhcp_options_exist(host):
+    res = host.run('ovn-nbctl -f json --columns _uuid '
+                   'find dhcp_options cidr=10.0.0.0/24')
+    data = json.loads(res.stdout)
+    data = dict(data['data'][0])
+
+    assert 'uuid' in data
+
+
+@pytest.mark.parametrize('port', ['port1', 'port2', 'port3'])
+def test_port_has_dhcp_options(host, port):
+    res = host.run('ovn-nbctl -f json --columns _uuid '
+                   'find dhcp_options cidr=10.0.0.0/24')
+    data = json.loads(res.stdout)
+    data = dict(data['data'][0])
+    options_uuid = data['uuid']
+
+    res = host.run('ovn-nbctl -f json --columns dhcpv4_options '
+                   f'list logical_switch_port {port}')
+    data = json.loads(res.stdout)
+    data = dict(data['data'][0])
+    assert data['uuid'] == options_uuid
